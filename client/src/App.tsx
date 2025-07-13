4b8d110c4bdaadc97f86045ab9296df20585e053
Oosla <ooslaava@gmail.com>
2025-07-13 15:28:20 +0300

    main: init frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+import logo from './logo.svg';
+
+
+function App() {
+  const [count, setCount] = useState()
+
+  return (
+    <div className="App">
+      <h1>hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
