e128a049ce7123f8235948b9ff8bde7d19e1befd
oosla <ooslaava@gmail.com>
2025-11-23 10:58:51 +0300

    dfkjs

--- client/src/elements/Card.tsx
+++ client/src/elements/Card.tsx
@@ -11,7 +11,7 @@ interface ICardProps {
 
 export default function Card(props: ICardProps) {
     return (
-        <Link to={(props.isActor ? ACTORS : EMPLOYEES) + "/" + props.person.id} className="card_main">
+        <Link to={`${props.isActor ? ACTORS : EMPLOYEES}/${props.person.id}`} className="card_main">
             <img className="card_avatar" src={props.person.avatarUrl} alt={`${props.person.firstName} ${props.person.lastName}`} />
             <h1 className="card_name">
                 {props.person.firstName} {props.person.lastName}
